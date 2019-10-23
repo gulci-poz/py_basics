4954a703c4767b0f4c6ca0ad8e34d67170ebd580
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2019-10-23 06:11:43 +0200

    time for lists

--- 14_nested_loops.py
+++ 14_nested_loops.py
@@ -1,1 +1,20 @@
-# 1h48min
+for x in range(4):
+    for y in range(4):
+        print(f'({x}, {y})')
+
+f_shape = [5, 2, 5, 2, 2]
+l_shape = [2, 2, 2, 2, 5]
+
+# for value in f_shape:
+#     print('x' * value)
+
+# for value in f_shape:
+#     output = ''
+#     for x in range(value):
+#         output += 'x'
+#     print(output)
+
+for value in l_shape:
+    for x in range(value):
+        print('x', end='')
+    print()
